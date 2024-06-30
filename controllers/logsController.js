6b3a380748a2a7acec3f13bad58b6712903523eb
AntCannon <anthonycannonier@gmail.com>
2024-06-30 16:52:18 -0400

    add GET /logs/:id

--- controllers/logsController.js
+++ controllers/logsController.js
@@ -2,8 +2,20 @@ const express = require('express')
 const logsController = express.Router()
 const logModel = require('../models/log')
 
+// index
 logsController.get('/', (req, res) => {
   res.json(logModel)
+})
+
+// show 
+logsController.get('/:id', (req, res) => {
+  const logId = +req.params.id
+  const logById = logModel.find(log => log.id === logId)
+  if (logById) {
+    res.status(201).json(logById)
+  } else {
+      res.redirect("*")
+  }
 })
 
 module.exports = logsController
