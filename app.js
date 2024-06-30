552e56c692a31a21114787630dd2219ac139a9bf
AntCannon <anthonycannonier@gmail.com>
2024-06-30 16:36:37 -0400

    add 404 route

--- app.js
+++ app.js
@@ -12,4 +12,9 @@ app.get('/', (req, res) => {
 // routes
 app.use('/logs', logsController)
 
+// 404
+app.use('*', (req, res) => {
+  res.status(404).json({error: 'No page found'})
+})
+
 module.exports = app
